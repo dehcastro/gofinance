85e19aada80f57f9092378c4379fa40c791dc93e
André de Castro <andre_vc@hotmail.com>
2022-08-16 23:41:47 -0300

    wrap App with ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
