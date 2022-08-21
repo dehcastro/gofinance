44f7c29e75b323efe7c16312fa6af30fe5c7e56c
André de Castro <andre_vc@hotmail.com>
2022-08-21 15:26:08 -0300

    fix Button type errors

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -3,10 +3,11 @@ import { Container, Title } from "./styles";
 
 interface Props extends RectButtonProps {
   title: string;
+  onPress: () => void;
 }
 
-export const Button = ({ title, ...rest }: Props) => (
-  <Container {...rest}>
+export const Button = ({ title, onPress, ...rest }: Props) => (
+  <Container {...rest} onPress={onPress}>
     <Title>{title}</Title>
   </Container>
 );
